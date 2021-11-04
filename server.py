0823389d1bae5ffc7141aa195caa933c23562cef
João Rosário <joaobrosario@Joaos-MacBook-Air.local>
2021-11-04 15:53:57 +0000

    added some comments

--- server.py
+++ server.py
@@ -53,10 +53,12 @@ def main_loop():
     poller.register(pub_socket, zmq.POLLIN)
     poller.register(sub_socket, zmq.POLLIN)
 
-
     while True:
+
+        # Create a dictionary with the sockets we created
         sockets = dict(poller.poll()) # we can setup a timeout for exiting blocking calls poll(1000) will block for 1s an then timeout
         
+        # Using the socket as a key we can find if the socket has content to poll 
         if(pub_socket in sockets and sockets[pub_socket] == zmq.POLLIN):
             pub_message = pub_socket.recv(zmq.NOBLOCK)
             print("Received message from a publisher: {}".format(pub_message))
@@ -67,24 +69,20 @@ def main_loop():
             print("Received message from a subscriber: {}".format(sub_message))
             sub_socket.send(b"OK")
 
-        
     return
 
-
+# Create folders and files where content will be saved
 def create_program_files():
     if not os.path.exists(program_files_dir):
         os.makedirs(program_files_dir)
     
-    
-    
-
+# Call the main loop and startup functions
 def start():
     print("Starting server...")
     create_program_files()
     main_loop()
 
  
-
 # A bit of cleanup code in case we use ctrl-c to end the server
 def handler(signum, frame):
     msg = "Do you really want to exit? y/n "
@@ -100,5 +98,5 @@ def handler(signum, frame):
  
 signal.signal(signal.SIGINT, handler)
 
-
+# Start the server
 start()
