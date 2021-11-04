43e12268a99db829af2e3e8c9f2f8802fef92dbd
João Rosário <joaobrosario@Joaos-MacBook-Air.local>
2021-11-04 16:02:09 +0000

    removed unused imports

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import asyncio
 import os
 import signal
 
-from zmq.backend import zmq_poll
-
 # peers will have to connect to the server in order to get messages published on a certain topic
 # peers will have an unique ID so that the server can keep track of each peer individually
 
