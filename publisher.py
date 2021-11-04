1d2aabda496da7ffe38da2bbac60fa5dc8ae37a9
João Rosário <joaobrosario@Joaos-MacBook-Air.local>
2021-11-04 15:16:05 +0000

    polling done

--- publisher.py
+++ publisher.py
@@ -2,10 +2,11 @@ import sys
 import zmq
 import asyncio
 
-port = "5556"
-if len(sys.argv) > 1:
-    port =  sys.argv[1]
-    int(port)
+port = "5556" # for now default port for publishers is 5556
+
+# if len(sys.argv) > 1:
+#     port =  sys.argv[1]
+#     int(port)
 
 
 # put() publishes a message on a topic
@@ -18,12 +19,14 @@ def put(message, topic = "NULL"):
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:{}".format(port))
     socket.send("{}".format(message).encode())
-    # socket.close() is called when garbage collection happens but we should still call it 
+    # socket.close() is called when garbage collection happens but we should still call it
     return
 
 def main_loop():
     #this loop will listen to keyboard inputs and proceed accordingly
     put("teste")
+
+    
     return
 
 main_loop()
